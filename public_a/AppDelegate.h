70b3f0c4cd2179fb579a97d5944fcbcab8f5fe5e
william <williamyzd@163.com>
2017-04-24 09:38:53 +0800

    Initial Commit

---
+++ public_a/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  public_a
+//
+//  Created by william on 2017/4/24.
+//  Copyright © 2017年 william. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ public_a/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  public_a
+//
+//  Created by william on 2017/4/24.
+//  Copyright © 2017年 william. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
